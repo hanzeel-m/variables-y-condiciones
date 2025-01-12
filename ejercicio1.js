d1db5013883dea9b47708883ece4a1f3e147a005
hanzeel <martinezhanzeel@gmail.com>
2025-01-12 18:51:37 -0500

    Variables

---
+++ ejercicio1.js
@@ -0,0 +1,15 @@
+/*
+    Crear un programa que guade en una variable el nombre, apellido, edad y retornar lo siguiente:ç
+
+    Hola Jonnnier Martinez, bienvenido a mi programa. Tienes 15 años
+
+    VARIBLES: var, let y const 
+*/
+const nombre = "Jonnier"
+const apellido = "Martinez"
+const edad = 15
+
+const mensaje = "Hola"+ " " + nombre+" "+apellido+" "+"Bienvenido a mi programa. tienes "+ edad+" "+"Años"
+
+
+console.log(mensaje)
---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * variables y condicionlaes 
+ */
+//variables 
+var primer_nombre = "carlos";
+var apellido = "martinez"
+var segundo_apellido = "sanchez"
+var segundo_nombre = " david"
+console.log(segundo_nombre)
+console.log(apellido)
+var edad = "14"
+console.log(segundo_apellido) 
+console.log(edad)
+console.log("edad:" + " "+ edad)
+
+//console.log(primer_nombre)
+//nsole.log(edad)
+//console.log("edad:" + " " + edad)
+//edad: 14
