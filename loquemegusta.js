491d9955614a14e55821ed93c123a49b2ea0071a
hanzeel <martinezhanzeel@gmail.com>
2025-01-12 19:43:31 -0500

    lo que me gusta

---
+++ loquemegusta.js
@@ -0,0 +1,23 @@
+//Soy Hanzeel, me gusta el futbol, mi judador favorito es (cristiano) la musica y se tocar guitarra
+// variables: nombre  jugador-fav
+ 
+const nombre = "hanzeel"
+const jugador_fav = "CR7"
+const equipo = "madrid"
+const numero_champions = 15
+const gana_siempre = true
+const pierde_siempre = false
+
+if (equipo === "madrid") {
+    console.log("estas en el equipo perfecto")
+}else{
+    console.log("estas en el peor equipo; ", equipo)
+}
+const edad = 13
+
+if(edad<18){
+console.log("estas joven")
+
+}else{
+console.log("estas viejo")    
+}
